b6ce31690da3bddaafa973698b7af0e77b7a7270
pavlobarabah <pavlobarabah@gmail.com>
2024-03-12 15:48:58 +0200

    started work on the /users endpoint

--- src/main/java/org/tinder/App.java
+++ src/main/java/org/tinder/App.java
@@ -1,13 +1,22 @@
-package org.example;
+package org.tinder;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.tinder.servlets.TestServlet;
+import org.tinder.servlets.UsersServlet;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        ServletContextHandler handler = new ServletContextHandler();
+
+        handler.addServlet(TestServlet.class,"/users");
+        handler.addServlet(new ServletHolder(new UsersServlet("templates")), "/users/*");
+
+        server.setHandler(handler);
+        server.start();
+        server.join();
     }
 }
