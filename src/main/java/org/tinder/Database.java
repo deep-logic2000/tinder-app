d93d47d27cb57c82961ad535517ebcfb7c9586fc
Kirill Nazarov <urij7762@gmail.com>
2024-03-17 20:45:53 +0200

    Connected Db & made tables

---
+++ src/main/java/org/tinder/Database.java
@@ -0,0 +1,17 @@
+package org.tinder;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+  public static Connection mkConn() throws SQLException {
+    return DriverManager.getConnection(
+      "jdbc:postgresql://localhost:5432/tinderDB",
+      "postgres",
+      "pg123456"
+    );
+  }
+
+}
