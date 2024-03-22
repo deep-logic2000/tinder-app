4543ff6dab2ab1e2588c245354641e9dc07e4b31
Kirill N <95580392+deep-logic2000@users.noreply.github.com>
2024-03-22 20:40:01 +0200

    Fix styles path (#17)
    * Fix styles path
    
    * Fix styles path
    
    ---------
    
    Co-authored-by: Kirill Nazarov <urij7762@gmail.com>

--- src/main/java/org/tinder/servlets/CssServlet.java
+++ src/main/java/org/tinder/servlets/CssServlet.java
@@ -9,10 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class CssServlet extends HttpServlet {
     private final String root;
@@ -23,16 +25,20 @@ public class CssServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String rqName = "/templates" + req.getRequestURI();
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        System.out.println("rqName: " + rqName);
 
-        String prefix = ResourceOps.resourceUnsafe(root);
-        String fileName = req.getPathInfo();
-        String fullName = prefix + fileName;
+        try (ServletOutputStream os = resp.getOutputStream();
+           InputStream grs = classLoader.getResourceAsStream(rqName.substring(1))) {
 
-        if (!new File(fullName).exists()) {
+            byte[] bytes = grs.readAllBytes();
+            System.out.println(Arrays.toString(bytes));
+
+            os.write(bytes);
+
+        } catch (NullPointerException | IOException ex) {
             resp.setStatus(404);
-        } else try (ServletOutputStream os = resp.getOutputStream()) {
-            Path path = Paths.get(fullName);
-            Files.copy(path, os);
         }
     }
 }
